9007e17def886a2d3292e34c8bb462909884e4de
Raphael Nestler <raphael.nestler@gmail.com>
2021-06-08 21:19:03 +0200

    Add English insults from SoMI (#38)

--- src/replies.ts
+++ src/replies.ts
@@ -6,7 +6,73 @@ import * as FuzzySet from 'fuzzyset.js';
  */
 const insults: Map<string, string[]> = new Map([
 
-  // The Secret of Monkey Island
+  // The Secret of Monkey Island English
+  [
+    'You fight like a dairy Farmer!',
+    ['How appropriate. You fight like a cow!'],
+  ],
+  [
+    'This is the END for you, you gutter crawling cur!',
+    ['And I’ve got a little TIP for you, get the POINT?'],
+  ],
+  [
+    'I’ve spoken with apes more polite than you!',
+    ['I’m glad to hear you attended your family reunion!'],
+  ],
+  [
+    'Soon you’ll be wearing my sword like a shish kebab!',
+    ['First you’d better stop waving it like a feather duster.'],
+  ],
+  [
+    'People fall at my feet when they see me coming!',
+    ['Even BEFORE they smell your breath?'],
+  ],
+  [
+    'I’m not going to take your insolence sitting down!',
+    ['Your hemorrhoids are flaring up again eh?'],
+  ],
+  [
+    'I once owned a dog that was smarter than you.',
+    ['He must have taught you everything you know.'],
+  ],
+  [
+    'Nobody’s ever drawn blood from me and nobody ever will.',
+    ['You run THAT fast?'],
+  ],
+  [
+    'Have you stopped wearing diapers yet?',
+    ['Why? Did you want to borrow one?'],
+  ],
+  [
+    'There are no words for how disgusting you are.',
+    ['Yes there are. You just never learned them.'],
+  ],
+  [
+    'You make me want to puke.',
+    ['You make me think somebody already did.'],
+  ],
+  [
+    'My handkerchief will wipe up your blood!',
+    ['So you got that job as janitor, after all.'],
+  ],
+  [
+    'I got this scar on my face during a mighty struggle!',
+    ['I hope now you’ve learned to stop picking your nose.'],
+  ],
+  [
+    'I’ve heard you are a contemptible sneak.',
+    ['Too bad no one’s ever heard of YOU at all.'],
+  ],
+  [
+    'You’re no match for my brains, you poor fool.',
+    ['I’d be in real trouble if you ever used them.'],
+  ],
+  [
+    'You have the manners of a beggar.',
+    ['I wanted to make sure you’d feel comfortable with me.'],
+  ],
+
+  // The Secret of Monkey Island German
   [
     'Du kämpfst wie ein dummer Bauer.',
     ['Wie passend. Du kämpfst wie eine Kuh.'],
